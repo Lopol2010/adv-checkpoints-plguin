e6711e4de701de77f1995c5267fa041bd4fdb817
Lopol2010 <erizhov7@gmail.com>
2017-08-12 02:36:14 +0300

    Learn git

--- update-files.ps1
+++
@@ -1,2 +0,0 @@
-Copy-Item 'C:\Program Files (x86)\steam\steamapps\common\Half-Life\cstrike\addons\amxmodx\scripting\advcheckpoints.sma' '.\' -Force
-Copy-Item 'C:\Program Files (x86)\steam\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins\advcheckpoints.amxx' '.\' -Force
